867781c416c225dff4698496efa0de8dd2d41bfc
lukaszci <lukasz@coffeemeetsbagel.com>
2018-06-13 10:53:13 -0700

    starting work on canvas

--- Assignment4/Hangman.java
+++ Assignment4/Hangman.java
@@ -9,64 +9,96 @@ import acm.graphics.*;
 import acm.program.*;
 import acm.util.*;
 import java.awt.*;
+import java.util.Objects;
 
 public class Hangman extends ConsoleProgram {
 	private static final int LIFE_COUNT = 8;
 	private static final long serialVersionUID = 1L;
-	private HangmanLexicon allWords;
-	private char givenLetter;
-	private String selectedWord;
 	private RandomGenerator rgen = RandomGenerator.getInstance();
+	private HangmanLexicon lexicon;
+	private HangmanCanvas canvas;
+	private String selectedWord;
+	private String givenLetter;
 	private int remainingLives;
 	private int remainingCharacters;
-	String givenWord;
+	private char extractedChar;
 
     public void run() {
+    		init();
+    		canvas.reset();
     		selectWord();
-    		while (remainingLives > 0 && remainingCharacters > 0)
-		selectedWord = readLine("Give me a letter: ");
-			for(int i=0; i< selectedWord.length(); i++) {
-				int givenLetter = selectedWord.indexOf(i);
-				System.out.println(givenLetter);
-					if (givenLetter != (-1) ) {
-						userGuessedRight(); 
-					} else {
-						userGuessedWrong();
-					}
-			}
-			if (remainingCharacters == 0) {
-				userWon();
-			}
+    		while (remainingLives > 0 && remainingCharacters > 0) {
+    			givenLetter = readLine("Give me a letter: ").toUpperCase();
+			int letterPosition = selectedWord.indexOf(givenLetter);
+			System.out.println(letterPosition);
+			if (letterPosition != (-1) && signIsLetter(givenLetter) == true) {
+					userGuessedRight(); 
+				} 
+			else if(givenLetter.length() > 1  || signIsLetter(givenLetter) == false) {
+				userGaveIllegalCombinationOfSigns();
+				}
 			else {
-				userLost();
+				userGuessedWrong();
 			}
+    		}
+    		if (remainingCharacters == 0) {
+				userWon();
+    		} else {
+				userLost();	
+    		}
+    		canvas.reset();
 	}
     
     public void selectWord() {
-    		allWords = new HangmanLexicon();
-    		int randomWord = rgen.nextInt(0, (allWords.getWordCount() - 1));
-    		selectedWord = allWords.getWord(randomWord);
+    		lexicon = new HangmanLexicon();
+    		int randomWord = rgen.nextInt(0, (lexicon.getWordCount() - 1));
+    		selectedWord = lexicon.getWord(randomWord);
     		remainingCharacters = selectedWord.length();
+    		System.out.println("The word is " + remainingCharacters + " letters long");
     		remainingLives = LIFE_COUNT;
+    		System.out.println("You have " + remainingLives + " lives");
     }
     
     public void userGuessedRight() {
-    		int sameLetters = 0;//placeholder - need to add a function that calculates occurrences of the same letter
-    		remainingCharacters = remainingCharacters - sameLetters;
-    		System.out.println("Bravo, this word contains this letter");
+    		if (Objects.equals(givenLetter, selectedWord)) {
+    			remainingCharacters = 0;
+    			System.out.println(remainingCharacters);
+    		} else {
+    			int sameLetters = 0;//placeholder - need to add a function that calculates occurrences of the same letter
+        		remainingCharacters = remainingCharacters - 1;
+        		System.out.println("Bravo, you have " + remainingCharacters + " letters to guess");
+    		}
     }
     
     public void userGuessedWrong() {
+    		extractLetter();
+    		canvas.noteIncorrectGuess(extractedChar);
     		remainingLives = remainingLives -1;
-    		System.out.println("no bueno, letter is not here");
+    		System.out.println("no bueno, letter is not here. You have " + remainingLives + " lives left");
     }
     
+    public void userGaveIllegalCombinationOfSigns() {
+		System.out.println("no bueno, You can only guess one sign at a time or the entire word. The only allowed signs are letters of latin alphabet and you can not leave the field empty!");
+}
+    
     public void userWon() {
-    		System.out.println("Bravo, you won the game");
+    		System.out.println("Bravo, you won the game. The word was obviously " + selectedWord);
     }
     
     public void userLost() {
-    		System.out.println("Bravo, you lost");
+    		System.out.println("Bravo, you lost. The word was " + selectedWord);
     }
-
+    
+    public boolean signIsLetter(String givenLetter) {
+        return givenLetter.matches("[a-zA-Z]+");
+    }
+    
+    public void init() {
+    	 canvas = new HangmanCanvas();
+    	 add(canvas);
+    	}
+    
+    public void extractLetter() {
+    		extractedChar = givenLetter.charAt(0);
+    }
 }
--- Assignment4/HangmanCanvas.java
+++ Assignment4/HangmanCanvas.java
@@ -7,10 +7,13 @@
 import acm.graphics.*;
 
 public class HangmanCanvas extends GCanvas {
+	private GOval head;
+	private GLabel guessed;
+	private String guessedSoFar = "";
 
 /** Resets the display so that only the scaffold appears */
 	public void reset() {
-		/* You fill this in */
+		addScaffold();
 	}
 
 /**
@@ -29,7 +32,97 @@ public class HangmanCanvas extends GCanvas {
  * guesses that appears at the bottom of the window.
  */
 	public void noteIncorrectGuess(char letter) {
-		/* You fill this in */
+		guessedSoFar = guessedSoFar + letter;
+		listIncorrectLetters();
+		/*System.out.println(guessedSoFar.length());
+		guessed = new GLabel(guessedSoFar + letter);
+		System.out.println(guessedSoFar.length());*/
+		if (guessedSoFar.length() == 1) {
+			addHead();
+		}
+		if (guessedSoFar.length() == 2) {
+			addBody();
+		}
+		if (guessedSoFar.length() == 3) {
+			addLeftArm();
+		}
+		if (guessedSoFar.length() == 4) {
+			addRightArm();
+		}
+		if (guessedSoFar.length() == 5) {
+			addLeftLeg();
+		}
+		if (guessedSoFar.length() == 6) {
+			addRightLeg();
+		}
+		if (guessedSoFar.length() == 7) {
+			addLeftFoot();
+		}
+		if (guessedSoFar.length() == 8) {
+			addRightFoot();
+		}
+		
+ 	}
+	
+	private void addScaffold() {
+		GLine pole = new GLine(getWidth()/4,getHeight()/4,getWidth()/4,getHeight()/4*3);
+		add(pole);
+		GLine beam = new GLine(222,212,211,112);
+		add(beam);
+	}
+	
+	private void addRope() {
+		GLine rope = new GLine(1,2,1,2);
+		add(rope);
+	}
+	
+	private void addHead() {
+		GOval head = new GOval(100,200,100,200);
+		add(head);
+	}
+	
+	private void addBody() {
+		GLine hips = new GLine(50,20,40,70);
+		add(hips);
+		GLine spine = new GLine(20, 50, 40, 70);
+		add(hips);
+		GLine shoulders = new GLine(70, 50, 40, 20);
+		add(hips);
+	}
+	
+	private void addLeftArm() {
+		GLine lArm = new GLine(1,2,1,2);
+		add(lArm);
+	}
+	
+	private void addRightArm() {
+		GLine rArm = new GLine(1,2,1,2);
+		add(rArm);
+	}
+	
+	private void addLeftLeg() {
+		GLine lLeg = new GLine(1,2,1,2);
+		add(lLeg);
+	}
+	
+	private void addRightLeg() {
+		GLine rLeg = new GLine(1,2,1,2);
+		add(rLeg);
+	}
+	
+	private void addLeftFoot() {
+		GLine lFoot = new GLine(1,2,1,2);
+		add(lFoot);
+	}
+	
+	private void addRightFoot() {
+		GLine rFoot = new GLine(1,2,1,2);
+		add(rFoot);
+	}
+	
+	private void listIncorrectLetters() {
+		guessed = new GLabel(guessedSoFar,50,200);
+		add(guessed);
 	}
 
 /* Constants for the simple version of the picture (in pixels) */
@@ -44,5 +137,4 @@ public class HangmanCanvas extends GCanvas {
 	private static final int HIP_WIDTH = 36;
 	private static final int LEG_LENGTH = 108;
 	private static final int FOOT_LENGTH = 28;
-
 }
