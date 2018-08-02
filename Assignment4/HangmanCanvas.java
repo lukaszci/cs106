740c4c66ebc2e2db71e0347da92b6510514d0d90
lukaszci <lukasz@coffeemeetsbagel.com>
2018-08-02 11:28:42 -0700

    finished hangman

--- Assignment4/HangmanCanvas.java
+++ Assignment4/HangmanCanvas.java
@@ -7,122 +7,158 @@
 import acm.graphics.*;
 
 public class HangmanCanvas extends GCanvas {
-	private GOval head;
-	private GLabel guessed;
-	private String guessedSoFar = "";
+	private String guessedWrongSoFar = "";
 
 /** Resets the display so that only the scaffold appears */
 	public void reset() {
 		addScaffold();
 	}
+	
+	/**
+	 * Updates the word on the screen to correspond to the current
+	 * state of the game.  The argument string shows what letters have
+	 * been guessed so far; unguessed letters are indicated by hyphens.
+	 */
 
-/**
- * Updates the word on the screen to correspond to the current
- * state of the game.  The argument string shows what letters have
- * been guessed so far; unguessed letters are indicated by hyphens.
- */
-	public void displayWord(String word) {
-		/* You fill this in */
+	public void noteCorrectGuess(String g) {
+		double x = getWidth()/4;
+		double y = getHeight() - HEAD_RADIUS*2;
+		GLabel correct = new GLabel(g, x, y);
+		correct.setFont("Halvetica-24");
+		if (getElementAt(x,y) != null){
+			remove(getElementAt(x,y));
+		}
+		add(correct);
 	}
-
-/**
- * Updates the display to correspond to an incorrect guess by the
- * user.  Calling this method causes the next body part to appear
- * on the scaffold and adds the letter to the list of incorrect
- * guesses that appears at the bottom of the window.
- */
+	/**
+	 * Updates the display to correspond to an incorrect guess by the
+	 * user.  Calling this method causes the next body part to appear
+	 * on the scaffold and adds the letter to the list of incorrect
+	 * guesses that appears at the bottom of the window.
+	 */
+	
 	public void noteIncorrectGuess(char letter) {
-		guessedSoFar = guessedSoFar + letter;
+		guessedWrongSoFar = guessedWrongSoFar + letter;
 		listIncorrectLetters();
-		/*System.out.println(guessedSoFar.length());
-		guessed = new GLabel(guessedSoFar + letter);
-		System.out.println(guessedSoFar.length());*/
-		if (guessedSoFar.length() == 1) {
+		if (guessedWrongSoFar.length() == 1) {
 			addHead();
 		}
-		if (guessedSoFar.length() == 2) {
+		if (guessedWrongSoFar.length() == 2) {
 			addBody();
 		}
-		if (guessedSoFar.length() == 3) {
+		if (guessedWrongSoFar.length() == 3) {
 			addLeftArm();
 		}
-		if (guessedSoFar.length() == 4) {
+		if (guessedWrongSoFar.length() == 4) {
 			addRightArm();
 		}
-		if (guessedSoFar.length() == 5) {
+		if (guessedWrongSoFar.length() == 5) {
 			addLeftLeg();
 		}
-		if (guessedSoFar.length() == 6) {
+		if (guessedWrongSoFar.length() == 6) {
 			addRightLeg();
 		}
-		if (guessedSoFar.length() == 7) {
+		if (guessedWrongSoFar.length() == 7) {
 			addLeftFoot();
 		}
-		if (guessedSoFar.length() == 8) {
+		if (guessedWrongSoFar.length() == 8) {
 			addRightFoot();
 		}
 		
  	}
 	
-	private void addScaffold() {
-		GLine pole = new GLine(getWidth()/4,getHeight()/4,getWidth()/4,getHeight()/4*3);
-		add(pole);
-		GLine beam = new GLine(222,212,211,112);
+	public void addScaffold() {
+		double scaffoldTopX = getWidth()/4 - UPPER_ARM_LENGTH;
+		double scaffoldTopY = getHeight()/2  - BODY_LENGTH - HEAD_RADIUS*2 - ROPE_LENGTH;
+		double scaffoldBottomY = scaffoldTopY + SCAFFOLD_HEIGHT;
+		GLine scaffold= new GLine (scaffoldTopX, scaffoldTopY, scaffoldTopX, scaffoldBottomY);
+		add(scaffold);
+		double beamRightX = scaffoldTopX + BEAM_LENGTH;
+		GLine beam = new GLine(scaffoldTopX, scaffoldTopY, beamRightX, scaffoldTopY);
 		add(beam);
-	}
-	
-	private void addRope() {
-		GLine rope = new GLine(1,2,1,2);
+		double ropeBottomY = scaffoldTopY + ROPE_LENGTH;
+		GLine rope = new GLine (beamRightX, scaffoldTopY, beamRightX, ropeBottomY);
 		add(rope);
 	}
 	
 	private void addHead() {
-		GOval head = new GOval(100,200,100,200);
+		double x = getWidth()/2 - UPPER_ARM_LENGTH + BEAM_LENGTH - HEAD_RADIUS;
+		double y = getHeight()/2 - BODY_LENGTH - HEAD_RADIUS*2;
+		GOval head = new GOval (x, y, HEAD_RADIUS*2, HEAD_RADIUS*2);
 		add(head);
 	}
 	
 	private void addBody() {
-		GLine hips = new GLine(50,20,40,70);
-		add(hips);
-		GLine spine = new GLine(20, 50, 40, 70);
-		add(hips);
-		GLine shoulders = new GLine(70, 50, 40, 20);
-		add(hips);
+		double x = getWidth()/2 + UPPER_ARM_LENGTH/2 + HEAD_RADIUS;
+		double topY = getHeight()/2 - BODY_LENGTH;
+		double bottomY = topY + BODY_LENGTH;
+		GLine body = new GLine (x, topY, x, bottomY);
+		add(body);
 	}
 	
 	private void addLeftArm() {
-		GLine lArm = new GLine(1,2,1,2);
-		add(lArm);
+		double armStartX = getWidth()/2 + UPPER_ARM_LENGTH/2 + HEAD_RADIUS;
+		double armEndX = getWidth()/2 + UPPER_ARM_LENGTH/2 + HEAD_RADIUS - UPPER_ARM_LENGTH;
+		double upperArmHeightY = getHeight()/2 - BODY_LENGTH + ARM_OFFSET_FROM_HEAD;
+		GLine upperLeftArm = new GLine (armStartX, upperArmHeightY, armEndX, upperArmHeightY);
+		add(upperLeftArm);
+		double lowerArmEndY = upperArmHeightY + LOWER_ARM_LENGTH;
+		GLine lowerLeftArm = new GLine (armEndX, upperArmHeightY, armEndX, lowerArmEndY);
+		add(lowerLeftArm);
 	}
 	
 	private void addRightArm() {
-		GLine rArm = new GLine(1,2,1,2);
-		add(rArm);
+		double armStartX = getWidth()/2 + UPPER_ARM_LENGTH/2 + HEAD_RADIUS;
+		double armEndX = getWidth()/2 + UPPER_ARM_LENGTH/2 + HEAD_RADIUS + UPPER_ARM_LENGTH;
+		double upperArmHeightY = getHeight()/2 - BODY_LENGTH + ARM_OFFSET_FROM_HEAD;
+		GLine upperRightArm = new GLine (armStartX, upperArmHeightY, armEndX, upperArmHeightY);
+		add(upperRightArm);
+		double lowerArmEndY = upperArmHeightY + LOWER_ARM_LENGTH;
+		GLine lowerRightArm = new GLine (armEndX, upperArmHeightY, armEndX, lowerArmEndY);
+		add(lowerRightArm);
 	}
 	
 	private void addLeftLeg() {
-		GLine lLeg = new GLine(1,2,1,2);
-		add(lLeg);
+		double hipStartX = getWidth()/2 + UPPER_ARM_LENGTH/2 + HEAD_RADIUS;
+		double hipEndX = hipStartX - HIP_WIDTH;
+		double hipHeightY = getHeight()/2;
+		GLine leftHip = new GLine(hipStartX, hipHeightY, hipEndX, hipHeightY);
+		add(leftHip);
+		double leftLegY = hipHeightY + LEG_LENGTH;
+		GLine leftLeg = new GLine(hipEndX, hipHeightY, hipEndX, leftLegY);
+		add(leftLeg);
 	}
 	
 	private void addRightLeg() {
-		GLine rLeg = new GLine(1,2,1,2);
-		add(rLeg);
+		double hipStartX = getWidth()/2 + UPPER_ARM_LENGTH/2 + HEAD_RADIUS;
+		double hipEndX = hipStartX + HIP_WIDTH;
+		double hipHeightY = getHeight()/2;
+		GLine leftHip = new GLine(hipStartX, hipHeightY, hipEndX, hipHeightY);
+		add(leftHip);
+		double leftLegEndY = hipHeightY + LEG_LENGTH;
+		GLine leftLeg = new GLine(hipEndX, hipHeightY, hipEndX, leftLegEndY);
+		add(leftLeg);
 	}
 	
 	private void addLeftFoot() {
-		GLine lFoot = new GLine(1,2,1,2);
-		add(lFoot);
+		double footStartX = getWidth()/2 + UPPER_ARM_LENGTH/2 + HEAD_RADIUS - HIP_WIDTH;
+		double footEndX = footStartX - FOOT_LENGTH;
+		double footHeightY = getHeight()/2 + LEG_LENGTH;
+		GLine leftFoot = new GLine(footStartX, footHeightY, footEndX, footHeightY);
+		add(leftFoot);
 	}
 	
 	private void addRightFoot() {
-		GLine rFoot = new GLine(1,2,1,2);
-		add(rFoot);
+		double footStartX = getWidth()/2 + UPPER_ARM_LENGTH/2 + HEAD_RADIUS + HIP_WIDTH;
+		double footEndX = footStartX + FOOT_LENGTH;
+		double footHeightY = getHeight()/2 + LEG_LENGTH;
+		GLine rightFoot = new GLine(footStartX, footHeightY, footEndX, footHeightY);
+		add(rightFoot);
 	}
 	
 	private void listIncorrectLetters() {
-		guessed = new GLabel(guessedSoFar,50,200);
-		add(guessed);
+		GLabel incorrect = new GLabel(guessedWrongSoFar, 100, 200);
+		add(incorrect);
 	}
 
 /* Constants for the simple version of the picture (in pixels) */
--- Assignment4/HangmanLexicon.java
+++ Assignment4/HangmanLexicon.java
@@ -19,16 +19,16 @@ public class HangmanLexicon {
 /** Returns the word at the specified index. */
 	public String getWord(int index) {
 		switch (index) {
-			case 0: return "BUOY";
-			case 1: return "COMPUTER";
+			case 0: return "LETTER";
+			case 1: return "TRUMPET";
 			case 2: return "CONNOISSEUR";
 			case 3: return "DEHYDRATE";
 			case 4: return "FUZZY";
 			case 5: return "HUBBUB";
 			case 6: return "KEYHOLE";
-			case 7: return "QUAGMIRE";
-			case 8: return "SLITHER";
-			case 9: return "ZIRCON";
+			case 7: return "SERVER";
+			case 8: return "MESMERISE";
+			case 9: return "MERMAID";
 			default: throw new ErrorException("getWord: Illegal index");
 		}
 	};
